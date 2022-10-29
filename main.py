92c119758d6274cc9cefa3a8fa46178817c92757
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-10-29 17:53:21 +0300

    Price determination

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ else:
     discount = 30
     discounted_price = float(price - (price * (discount / 100)))
     print(discounted_price)
+# Sale of alchohol to the customer:
+id_check = str(input("Show your id please : "))
+age_check = float(input("age : "))
+if id_check  == str("yes") and age_check >= 18:
+    print("Yes .You can.")
+else:
+    print("Sorry you cannot purchase.")
